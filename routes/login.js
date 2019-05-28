1df2093f3e5cf726fd1fea81fa7bfe881a6fec8c
David <david.solorzano@altia.es>
2019-05-28 16:46:41 +0200

    Usuario y token listos

---
+++ routes/login.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+var app = express();
+// Importar modelo de usuario
+var Usuario = require('../models/usuario');
+
+// Login
+app.post('/', (req, res, next) => {
+    var body = req.body;
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            })
+        }
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al login - email',
+                errors: err
+            })
+        }
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al login - password',
+                errors: err
+            })
+        }
+        // Crear un token
+        usuarioDB.password = ':)';
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioDB,
+            token: token,
+            id: usuarioDB.id
+        })
+    });
+
+
+});
+
+module.exports = app;
